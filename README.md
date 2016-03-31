fa7add9d4bcd34bc96755dbe08c0fac616f0bfa4
tobiasnickel <ross40k@googlemail.com>
2016-03-31 12:47:16 +0800

    update to be sync with npm, it was long time forgotten on github

--- README.md
+++ README.md
@@ -3,18 +3,21 @@ a very small xml parser in pure javascript
 
 This lib only provides one single method. **tXml()**
 
-1. this code is about 230 lines, can be easily extended 
-2. this code is 0.9kb minified + gzipped
-3. this code can running in a worker 
-4. this code is parsing at average the same speed as native DOMParser + potential to be faster
-5. this code is easy to read and good for study. 
-6. this code creates a domObject with minimal footprint, that is easy to traverse.
-7. this code has proved in different projects.
-8. this code can even parse handwritten XML that contains various errors
-
+1. this code is about 230 lines, can be easily extended. 
+2. this code is 0.9kb minified + gzipped.
+3. this code is 5 - 10 times faster then sax/xml2js.
+4. this code can running in a worker.
+5. this code is parsing at average the same speed as native DOMParser + potential to be faster.
+6. this code is easy to read and good for study. 
+7. this code creates a domObject with minimal footprint, that is easy to traverse.
+8. this code has proven in different projects, like RSS reader, openStreetmap, websites.
+9. this code can even parse handwritten XML that contains various errors.
+10. this code is working in client and server.
+11. this code is 100% covered by unit tests.
+  
 so, there are good reasons to give tXml.js a try. 
 
-### XML - features
+## XML - features
   1. tags
   2. childTags
   3. text-nodes
@@ -24,86 +27,92 @@ so, there are good reasons to give tXml.js a try.
   7. xmlComments (ignored)
   8. embedded CSS and Javascript
   9. HTML singleTag elements br, img, link, meta, hr
+  10. doctype definitions
+  11. xml namespaces
+  12. sync API for a sync process
+  13. getElementsById/-Class direct on the xmlString 
+  14. simplefy, similar to PHP's [SimpleXML](http://php.net/manual/en/book.simplexml.php)
+  15. filter, similar to underscore, as a alternative to CSS selectors
 
-### Installation
-In browser you load it how ever you want. for example as tag before you use it: <script src="tXml.js"></script>.
+## Installation
+In browser you load it how ever you want. For example as tag: <script src="tXml.js"></script>.
 
-In node, run **"npm install txml"** in your project. if necessary with **--save** parameter.
-and then in your script you require it by "var tXml = require('txml');"
+In node and browseryfy, run **"npm install txml"** in your project.
+and then in your script you require it by "var tXml = require('txml');
 
+## Methods
 
-### Methods
-**tXml** *(XML-string, options)*
-
-	1. **XML string** is the XML to parse.
-	2. **options** is optional 
-		options are: **searchId** an ID of some object. that can be queried. Using this is incredible fast. 
+### **tXml** *(XML-string, options)*
+1. **XML string** is the XML to parse.
+2. **options** is optional 
+		**searchId** an ID of some object. that can be queried. Using this is incredible fast. 
 		**filter** a method, to filter for interesting nodes, use it like Array.filter.
-		**simplify** to simplify the object, to an easier
-	example 1: tXml("<user is='great'><name>Tobias</name><familyName>Nickel</familyName><profession>Software Developer</profession><location>China</location><user>");
-	// will return an object like: [
-		{
-			"tagName": "user",
-			"attributes": {
-				"is": "great"
-			},
-			"children": [
-				{
-					"tagName": "name",
-					"children": [
-						"Tobias"
-					]
-				},
-				{
-					"tagName": "familyName",
-					"children": [
-						"Nickel"
-					]
-				},
-				{
-					"tagName": "profession",
-					"children": [
-						"Software Developer"
-					]
-				},
-				{
-					"tagName": "location",
-					"children": [
-						"China"
-					]
-				},
-				{
-					"tagName": "user",
-					"children": []
-				}
-			]
-		}
-	]	
+		**simplify** to simplify the object, to an easier access
+		
+	
+	EXAMPLE 1: tXml("<user is='great'><name>Tobias</name><familyName>Nickel</familyName><profession>Software Developer</profession><location>Shanghai / China</location></user>");
+
+	// will return an object like: 
+	[{
+		"tagName": "user",
+		"attributes": {
+			"is": "great"
+		},
+		"children": [{
+				"tagName": "name",
+				"children": [ "Tobias" ]
+			}, {
+				"tagName": "familyName",
+				"children": [ "Nickel" ]
+			}, {
+				"tagName": "profession",
+				"children": [ "Software Developer" ]
+			}, {
+				"tagName": "location",
+				"children": [ "Shanghai / China" ]
+			}
+		]
+	}]	
 		
 		
-	example 2: tXml("<user is='great'><name>Tobias</name><familyName>Nickel</familyName><profession>Software Developer</profession><location>China</location><user>",{simplify:1});
-	// will return an object like: {
+	EXAMLPLE 2: tXml("<user is='great'><name>Tobias</name><familyName>Nickel</familyName><profession>Software Developer</profession><location>Shanghai / China</location></user>",{simplify:1});
+	// will return an object like: 
+	{
 		"user": {
 			"name": "Tobias",
 			"familyName": "Nickel",
 			"profession": "Software Developer",
-			"location": "China",
-			"user": {},
+			"location": "Shanghai / China",
 			"_attributes": {
 				"is": "great"
 			}
 		}
 	}
 
-**tXml.simpify** *(tXml_DOM_Object)* this methid is used with the simplify parameter;
+### **tXml.simpify** *(tXml_DOM_Object)* 
+this methid is used with the simplify parameter;
+1. **tXml_DOM_Object** the object to simplify.
 
-  1. **tXml_DOM_Object** the object to simplify.
+### **tXml.filter** *(tXml_DOM_Object, f)* 
+this methid is used with the filter parameter, it is used like Array.filter.
+1. **tXml_DOM_Object** the object to filter.
+2. **f** a function that returns true if you want this elements in the result set.
 
-**tXml.filter** *(tXml_DOM_Object, f)* this methid is used with the filter parameter, it is used like Array.filter.
+### **tXml.getElementById** (xml,id) 
+to find an element by ID. if you are only interested for the information on, a specific node, this is easy and fast, because not the entire xml need to get parsed to a tDOM Object. returns the element not simplefied, you can do with tXml.simplify()
+1. **xml** the xml string to search in.
+2. **id** the id of the element to find
 
-  1. **tXml_DOM_Object** the object to filter.
-  2. **f** a function that returns true if you want this elements in the result set.
 
-### Developer
-[Tobias Nickel](http://tnickel.de/)  
+### **tXml.getElementsByClassName** (xml,className) 
+find the elements with the given class, without parsing the entire xml into a tDOM. so it is very fast and convenient. returns a list of elements. 
+1. **xml** the xml string to search in.
+2. **className** the className of the element to find
+
+
+
+
+
+## Developer
+[Tobias Nickel](http://tnickel.de/) German software developer in Shanghai. 
 ![alt text](https://avatars1.githubusercontent.com/u/4189801?s=150) 
--- tXml.js
+++ tXml.js
@@ -1,6 +1,11 @@
+// ==ClosureCompiler==
+// @output_file_name default.js
+// @compilation_level SIMPLE_OPTIMIZATIONS
+// ==/ClosureCompiler==
+
 /**
  * @author: Tobias Nickel
- * @date: 06.04.2015
+ * @created: 06.04.2015
  * I needed a small xmlparser chat can be used in a worker.
  */
 
@@ -8,13 +13,16 @@
  * parseXML / html into a DOM Object. with no validation and some failur tolerance
  * @params S {string} your XML to parse
  * @param options {object} all other options:
- *			searchId {string} the id of a single element, that should be returned. using this will increase the speed rapidly
- *			filter {function} filter method, as you know it from Array.filter. but is goes throw the DOM.
- *			simplefy {bool} to use tXml.simplefy.
+ * searchId {string} the id of a single element, that should be returned. using this will increase the speed rapidly
+ * filter {function} filter method, as you know it from Array.filter. but is goes throw the DOM.
+ * simplify {bool} to use tXml.simplify.
  */
+
 function tXml(S, options) {
     "use strict";
     options = options || {};
+
+    var pos = 0;
 
     var openBracket = "<";
     var openBracketCC = "<".charCodeAt(0);
@@ -30,23 +38,22 @@ function tXml(S, options) {
     var singleQuoteCC = "'".charCodeAt(0);
     var doubleQuote = '"';
     var doubleQuoteCC = '"'.charCodeAt(0);
-    
+
     /**
      * parsing a list of entries
      */
-    
+
     function parseChildren() {
         var children = [];
         while (S[pos]) {
             if (S.charCodeAt(pos) == openBracketCC) {
                 if (S.charCodeAt(pos + 1) === slashCC) {
-                    //while(S[pos]!=='>'){ pos++; }
                     pos = S.indexOf(closeBracket, pos);
                     return children;
                 } else if (S.charCodeAt(pos + 1) === exclamationCC) {
                     if (S.charCodeAt(pos + 2) == minusCC) {
                         //comment support
-                        while (!(S.charCodeAt(pos) === closeBracketCC && S.charCodeAt(pos - 1) == minusCC && S.charCodeAt(pos - 2) == minusCC && pos != -1)) {
+                        while (pos !== -1 && !(S.charCodeAt(pos) === closeBracketCC && S.charCodeAt(pos - 1) == minusCC && S.charCodeAt(pos - 2) == minusCC && pos != -1)) {
                             pos = S.indexOf(closeBracket, pos + 1);
                         }
                         if (pos === -1)
@@ -72,10 +79,11 @@ function tXml(S, options) {
         }
         return children;
     }
+
     /**
      *    returns the text outside of texts until the first '<'
      */
-    
+
     function parseText() {
         var start = pos;
         pos = S.indexOf(openBracket, pos) - 1;
@@ -87,7 +95,7 @@ function tXml(S, options) {
      *    returns text until the first nonAlphebetic letter
      */
     var nameSpacer = '\n\t>/= ';
-    
+
     function parseName() {
         var start = pos;
         while (nameSpacer.indexOf(S[pos]) === -1) {
@@ -100,11 +108,12 @@ function tXml(S, options) {
      * to parse children it uses the parseChildren again, that makes the parsing recursive
      */
     var NoChildNodes = ['img', 'br', 'input', 'meta', 'link'];
+
     function parseNode() {
         var node = {};
         pos++;
         node.tagName = parseName();
-        
+
         // parsing attributes
         var attrFound = false;
         while (S.charCodeAt(pos) !== closeBracketCC) {
@@ -125,13 +134,13 @@ function tXml(S, options) {
                 if (code === singleQuoteCC || code === doubleQuoteCC) {
                     var value = parseString();
                 } else {
-                    value = null ;
+                    value = null;
                     pos--;
                 }
                 node.attributes[name] = value;
             }
             pos++;
-        
+
         }
         // optional parsing of children
         if (S.charCodeAt(pos - 1) !== slashCC) {
@@ -152,100 +161,203 @@ function tXml(S, options) {
         }
         return node;
     }
+
     /**
      *    is parsing a string, that starts with a char and with the same usually  ' or "
      */
-    
+
     function parseString() {
         var startChar = S[pos];
         var startpos = ++pos;
         pos = S.indexOf(startChar, startpos)
         return S.slice(startpos, pos);
     }
-    function findId() {
-        return new RegExp('\s*id\s*=\s*[\'"]' + options.searchId + '[\'"]').exec(S).index;
+
+    /**
+     *
+     */
+
+    function findElements() {
+        var r = new RegExp('\\s' + options.attrName + '\\s*=[\'"]' + options.attrValue + '[\'"]').exec(S)
+        if (r) {
+            return r.index;
+        } else {
+            return -1;
+        }
     }
-    var out=null;
-    if (options.searchId) {
-        var pos = findId();
-        if (pos !== -1) {  
+
+    var out = null;
+    if (options.attrValue !== undefined) {
+        options.attrName = options.attrName || 'id';
+        var out = [];
+
+        while ((pos = findElements()) !== -1) {
             pos = S.lastIndexOf('<', pos);
             if (pos !== -1) {
-                out = parseNode();
+                out.push(parseNode());
             }
+            S = S.substr(pos);
+            pos = 0;
         }
-        return pos;
     } else {
-        var pos = 0;
         out = parseChildren();
     }
 
-    if(options.filter){
-        out = tXml.filter(out,options.filter);
+    if (options.filter) {
+        out = tXml.filter(out, options.filter);
     }
 
-    if(options.simplify){
-        out = tXml.simplefy(out);
+    if (options.simplify) {
+        out = tXml.simplify(out);
     }
     return out;
 }
 /**
  * transform the DomObject to an object that is like the object of PHPs simplexmp_load_*() methods.
  * this format helps you to write that is more likely to keep your programm working, even if there a small changes in the XML schema.
- * be aware, that it is not possible to reproduce the original xml from a simplefied version, because the order of elements is not saved.
+ * be aware, that it is not possible to reproduce the original xml from a simplified version, because the order of elements is not saved.
  * therefore your programm will be more flexible and easyer to read.
  *
  * @param {array} the childrenList
  */
-tXml.simplify = function simplefy(children) {
+tXml.simplify = function simplify(children) {
     var out = {};
-    
-    if(children.length === 1 && typeof children[0] == 'string')
-        return children[0];
+    if (!children.length) {
+        return '';
+    }
 
+    if (children.length === 1 && typeof children[0] == 'string') {
+        return children[0];
+    }
     // map each object
     children.forEach(function(child) {
-
+		if (typeof child !== 'object') {
+			return;
+		}
         if (!out[child.tagName])
             out[child.tagName] = [];
-        if (typeof child == 'object') {
-            var kids = tXml.simplefy(child.children);
-            out[child.tagName].push(kids);
-            if (child.attributes) {
-                kids._attributes = child.attributes;
-            }
-        }else{
-            out[child.tagName].push(child);
-        }
-    }
-    );
-    
+		var kids = tXml.simplify(child.children);
+		out[child.tagName].push(kids);
+		if (child.attributes) {
+			kids._attributes = child.attributes;
+		}
+    });
+
     for (var i in out) {
         if (out[i].length == 1) {
             out[i] = out[i][0];
         }
     }
-    
+
     return out;
 };
 
 /**
  * behaves the same way as Array.filter, if the filter method return true, the element is in the resultList
  * @params children{Array} the children of a node
- * @param f{function} the filter method 
+ * @param f{function} the filter method
  */
-tXml.filter = function(children,f){
-    var out=[];
-    children.forEach(function(child){
-        if(typeof(child) === 'object' && f(child))out.push(child);
-        if(child.children){
-            var kids = tXml.filter(child.children,f);
+tXml.filter = function(children, f) {
+    var out = [];
+    children.forEach(function(child) {
+        if (typeof(child) === 'object' && f(child)) out.push(child);
+        if (child.children) {
+            var kids = tXml.filter(child.children, f);
             out = out.concat(kids);
         }
     });
     return out;
 };
-if('object'!==typeof window){module.exports=tXml;}
+
+/**
+ * stringify a previously parsed string object.
+ * this is useful,
+ *  1. to remove whitespaces
+ * 2. to recreate xml data, with some changed data.
+ * @param O{tXMLDomObject} the object to Stringify
+ */
+tXml.stringify = function TOMObjToXML(O) {
+    var out = '';
+
+    function writeChildren(O) {
+        if (O)
+            for (var i = 0; i < O.length; i++) {
+                if (typeof O[i] == 'string') {
+                    out += O[i].trim();
+                } else {
+                    writeNode(O[i]);
+                }
+            }
+    }
+
+    function writeNode(N) {
+        out += "<" + N.tagName;
+        for (var i in N.attributes) {
+			if(N.attributes[i] === null){
+				out += ' ' + i;
+			}else if (N.attributes[i].indexOf('"') === -1) {
+                out += ' ' + i + '="' + N.attributes[i].trim() + '"';
+            } else {
+                out += ' ' + i + "='" + N.attributes[i].trim() + "'";
+            }
+        }
+        out += '>';
+        writeChildren(N.children);
+        out += '</' + N.tagName + '>';
+    }
+    writeChildren(O);
+
+    return out;
+};
+
+
+/**
+ * use this method to read the textcontent, of some node.
+ * It is great if you have mixed content like:
+ * this text has some <b>big</b> text and a <a href=''>link</a>
+ */
+tXml.toContentString = function(tDom) {
+    if (Array.isArray(tDom)) {
+        var out = '';
+        tDom.forEach(function(e) {
+            out += ' ' + tXml.toContentString(e);
+            out = out.trim();
+        });
+        return out;
+    } else if (typeof tDom === 'object') {
+        return tXml.toContentString(tDom.children)
+    } else {
+        return ' ' + tDom;
+    }
+};
+
+tXml.getElementById = function(S, id, simplified) {
+    var out = tXml(S, {
+        attrValue: id,
+        simplify: simplified
+    });
+    return simplified ? out : out[0];
+};
+/**
+ * A fast parsing method, that not realy finds by classname,
+ * more: the class attribute contains XXX
+ * @param
+ */
+tXml.getElementsByClassName = function(S, classname, simplified) {
+    return tXml(S, {
+        attrName: 'class',
+        attrValue: '[a-zA-Z0-9\-\s ]*' + classname + '[a-zA-Z0-9\-\s ]*',
+        simplify: simplified
+    });
+};
+
+if ('object' === typeof module) {
+    module.exports = tXml;
+}
+//console.clear();
+//console.log('here:',tXml.getElementById('<some><xml id="test">dada</xml><that id="test">value</that></some>','test'));
+//console.log('here:',tXml.getElementsByClassName('<some><xml id="test" class="sdf test jsalf">dada</xml><that id="test">value</that></some>','test'));
+
 /*
 console.clear();
 tXml(d,'content');
